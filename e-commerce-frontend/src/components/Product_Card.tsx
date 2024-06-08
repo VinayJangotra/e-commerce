1d18215eee3a7d9933197a993eff0c07e69d148f
VinayJangotra <109384165+VinayJangotra@users.noreply.github.com>
2024-06-08 22:55:50 +0530

    Home Page Frontend
    In this we create the frontend for the homepage adding the product card component in the Home page

---
+++ e-commerce-frontend/src/components/Product_Card.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { FaPlus } from 'react-icons/fa';
+type ProductsProps= {
+    productId:string;
+    photo:string;
+    name:string;
+    price:number;
+    stock:number;
+    handler:()=>void;
+}
+const server="jnsvolnfdsol";
+const Product_Card = ({productId,photo,name,price,stock,handler}:ProductsProps) => {
+  return (
+    <div className='product-card'>
+      <img src={photo} alt={name} />
+      <p>{name}</p>
+      <span>{price} </span>
+      <div>
+        <button onClick={()=>handler()}>
+            <FaPlus/>
+        </button>
+      </div>
+    </div>
+  )
+}
+export default Product_Card
