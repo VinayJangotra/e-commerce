6b82155d015615b4acef90b5ab453dbb431c7569
VinayJangotra <109384165+VinayJangotra@users.noreply.github.com>
2024-06-14 17:51:10 +0530

    Admin Authentication
    in this we can check that the user is admin or not

--- e-commerce-backend/src/routes/user.ts
+++ e-commerce-backend/src/routes/user.ts
@@ -1,10 +1,11 @@
 import  express  from "express";
 import { deleteUser, getAllUsers, getUser, newUser } from "../controllers/user";
+import { adminOnly } from "../middlewares/auth";
 const app = express.Router();
 app.post("/new",newUser);
 // routes = /api/v1/user/all
-app.get("/all",getAllUsers);
+app.get("/all",adminOnly,getAllUsers);
 // routes = /api/v1/user/dynamic id
 // This is the alternate way of writing the routes in which the path are same
-app.route("/:id").get(getUser).delete(deleteUser);
+app.route("/:id").get(getUser).delete(adminOnly, deleteUser);
 export default app;
