86397985a0a9b48cbaa0163f6d563a2935cba3df
VinayJangotra <109384165+VinayJangotra@users.noreply.github.com>
2024-06-21 14:06:24 +0530

    Pie Chart Api
    Inn this we add the pie chart API for the Admin

--- e-commerce-backend/dist/controllers/stats.js
+++ e-commerce-backend/dist/controllers/stats.js
@@ -155,6 +155,80 @@ exports.getDashBoardStats = (0, error_1.TryCatch)((req, res, next) => __awaiter(
         stats,
     });
 }));
-exports.getPieCharts = (0, error_1.TryCatch)(() => __awaiter(void 0, void 0, void 0, function* () { }));
+exports.getPieCharts = (0, error_1.TryCatch)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    let charts;
+    const key = "admin-pie-charts";
+    if (myCache.has(key))
+        charts = JSON.parse(myCache.get(key));
+    else {
+        const allOrderPromise = order_1.Order.find({}).select([
+            "total",
+            "discount",
+            "subtotal",
+            "tax",
+            "shippingCharges",
+        ]);
+        const [processingOrder, shippedOrder, deliveredOrder, categories, productsCount, outOfStock, allOrders, allUsers, adminUsers, customerUsers,] = yield Promise.all([
+            order_1.Order.countDocuments({ status: "Processing" }),
+            order_1.Order.countDocuments({ status: "Shipped" }),
+            order_1.Order.countDocuments({ status: "Delivered" }),
+            product_1.Product.distinct("category"),
+            product_1.Product.countDocuments(),
+            product_1.Product.countDocuments({ stock: 0 }),
+            allOrderPromise,
+            user_1.User.find({}).select(["dob"]),
+            user_1.User.countDocuments({ role: "admin" }),
+            user_1.User.countDocuments({ role: "user" }),
+        ]);
+        const orderFullfillment = {
+            processing: processingOrder,
+            shipped: shippedOrder,
+            delivered: deliveredOrder,
+        };
+        const productCategories = yield (0, features_1.getInventories)({
+            categories,
+            productsCount,
+        });
+        const stockAvailablity = {
+            inStock: productsCount - outOfStock,
+            outOfStock,
+        };
+        const grossIncome = allOrders.reduce((prev, order) => prev + (order.total || 0), 0);
+        const discount = allOrders.reduce((prev, order) => prev + (order.discount || 0), 0);
+        const productionCost = allOrders.reduce((prev, order) => prev + (order.shippingCharges || 0), 0);
+        const burnt = allOrders.reduce((prev, order) => prev + (order.tax || 0), 0);
+        const marketingCost = Math.round(grossIncome * (30 / 100));
+        const netMargin = grossIncome - discount - productionCost - burnt - marketingCost;
+        const revenueDistribution = {
+            netMargin,
+            discount,
+            productionCost,
+            burnt,
+            marketingCost,
+        };
+        const usersAgeGroup = {
+            teen: allUsers.filter((i) => i.age < 20).length,
+            adult: allUsers.filter((i) => i.age >= 20 && i.age < 40).length,
+            old: allUsers.filter((i) => i.age >= 40).length,
+        };
+        const adminCustomer = {
+            admin: adminUsers,
+            customer: customerUsers,
+        };
+        charts = {
+            orderFullfillment,
+            productCategories,
+            stockAvailablity,
+            revenueDistribution,
+            usersAgeGroup,
+            adminCustomer,
+        };
+        myCache.set(key, JSON.stringify(charts));
+    }
+    return res.status(200).json({
+        success: true,
+        charts,
+    });
+}));
 exports.getBarCharts = (0, error_1.TryCatch)(() => __awaiter(void 0, void 0, void 0, function* () { }));
 exports.getLineCharts = (0, error_1.TryCatch)(() => __awaiter(void 0, void 0, void 0, function* () { }));
