3aed3bbdc6a95b68fd3fa0c89421c4588d4d04d6
VinayJangotra <109384165+VinayJangotra@users.noreply.github.com>
2024-06-26 16:18:40 +0530

    Project Completes

--- e-commerce-backend/src/utils/utility-class.ts
+++
@@ -1,9 +0,0 @@
-class ErrorHandler extends Error{
-    statusCode:number;
-    constructor(message:string,statusCode:number){
-        super();
-        this.statusCode=statusCode;
-        this.message=message;
-    }
-}
- export default ErrorHandler;
